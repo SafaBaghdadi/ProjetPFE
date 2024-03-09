7f5ff796a0b916c1b60c20aa664b85de2792206a
safe baghdadi <baghdadisafa@outlook.com>
2024-03-09 16:22:54 +0100

    new branch

--- src/main/java/com/projetpfe/projetpfe/Repository/UserRepository.java
+++ src/main/java/com/projetpfe/projetpfe/Repository/UserRepository.java
@@ -5,7 +5,7 @@ import com.projetpfe.projetpfe.Models.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
-
+//interface
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
     Optional<UserEntity> findByUsername(String username);
     boolean existsByUsername(String username);
